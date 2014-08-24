151fc284e96f1b73ad9cdb8f4329256e3b40d704
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-08-24 12:25:43 +0200

    added support to save save model

--- classifier/svm_classifier_test.go
+++ classifier/svm_classifier_test.go
@@ -10,10 +10,15 @@ func TestSvmClassifier(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	err = classifier.TrainClassifier("2014_b_train.txt", "2014_b_dev.txt")
-	if err != nil {
-		t.Fatal(err)
-	}
+  err = classifier.TrainClassifier("2014_b_train.txt", "2014_b_dev.txt")
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  /*err = classifier.LoadClassifier("svm_model")
+  if err != nil {
+    t.Fatal(err)
+  }*/
 
 	err = classifier.TestClassifier("2014_b_test_gold.txt")
 	if err != nil {
