c9f70ad48f167d6c17f5bd19845f6571bf5a0a27
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-04-19 13:09:54 +0200

    extended docker deploy script

--- start_docker.sh
+++ start_docker.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-docker build --no-cache -t outyet .
-boot2docker up && $(boot2docker shellinit) 
-boot2docker ip
-# docker 
-docker run -p 8080:8080 -t outyet
-# docker ps
-# docker inspect hash
-# docker stop hash
+start() {
+  docker build --no-cache -t outyet .
+  boot2docker up && $(boot2docker shellinit) 
+  boot2docker ip
+  # Run a docker
+  docker run -p 8080:8080 -t outyet
+}
+
+info() {
+  # docker ps
+  docker ps
+  # docker inspect hash
+}
+
+stopall() {
+  # docker stop hash
+  docker stop $(docker ps -a -q)
+}
+
+cleanup() {
+  # Remove all containers
+  docker rm $(docker ps -a -q)
+  # Romve all images
+  docker rmi $(docker images -q)
+}
+
+case $1 in start|info|stopall|cleanup) "$1" ;; *) printf >&2 '%s: unknown command\n' "$1"; exit 1;; esac
